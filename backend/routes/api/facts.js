da1e72658b970c4f195a8b32ee221433bd9911b2
alixcaroline <alixcaroline13@gmail.com>
2019-04-27 13:57:18 +0200

    GET Route quote of the day

--- backend/routes/api/facts.js
+++ backend/routes/api/facts.js
@@ -2,10 +2,23 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const Fact = require('../../models/Fact');
+
 //@route GET api/facts/daily
 //@desc Tests facts route
 //@access Private
-router.get('/daily', (req, res) => res.json({ msg: 'facts works' }));
+router.get('/daily', (req, res) => {
+  //get todays day
+  const date = new Date();
+  const today = date.toString().slice(8, 10);
+  //find matching fact in database
+  Fact.findOne({ day: today }).then((err, fact) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(fact);
+    }
+  });
+});
 
 //@route POST api/facts/new
 //@desc post new fact route
