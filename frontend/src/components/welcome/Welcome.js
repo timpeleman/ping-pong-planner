aa8b120c7d62a02427e36bc7b4f1955681d2230b
Aisha El Salawi <aisha.elsalawi@aol.com>
2019-05-01 10:20:24 +0200

    update push

--- frontend/src/components/welcome/Welcome.js
+++ frontend/src/components/welcome/Welcome.js
@@ -1,12 +1,43 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Fragment from 'react-dom-fragment';
 
+//COMPONENTS 
+import Header from '../layout/Header';
 
 class Welcome extends Component {
     render() {
         return(
-            <h1>hello</h1>
+            <Fragment>
+                <Header/>
+                <ul>
+                    <li>eerste</li>
+                    <li>jeej het werkt</li>
+                    <li>qlmsdfjqmsldfjmqsdfjkmqks</li>
+                    <li>qlmsdfjqmsldfjmqsdfjkmqks</li>
+                    <li>qlmsdfjqmsldfjmqsdfjkmqks</li>
+                    <li>qlmsdfjqmsldfjmqsdfjkmqks</li>
+                    <li>qlmsdfjqmsldfjmqsdfjkmqks</li>
+                    <li>qlmsdfjqmsldfjmqsdfjkmqks</li>
+                    <li>qlmsdfjqmsldfjmqsdfjkmqks</li>
+                    <li>qlmsdfjqmsldfjmqsdfjkmqks</li>
+                    <li>qlmsdfjqmsldfjmqsdfjkmqks</li>
+                    <li>qlmsdfjqmsldfjmqsdfjkmqks</li>
+                    <li>qlmsdfjqmsldfjmqsdfjkmqks</li>
+                    <li>qlmsdfjqmsldfjmqsdfjkmqks</li>
+                    <li>qlmsdfjqmsldfjmqsdfjkmqks</li>
+                </ul>
+            </Fragment>
         )
     }
 }
 
 export default Welcome;
+
+
+{/* <Header/>
+                    <Switch>
+    <Route exact path="/Calendar" component={Calendar}/>
+    <Route exact path="/Profile" component={Profile}/>
+</Switch> 
+*/}
